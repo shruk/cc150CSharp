f39d703950ac2b88c8cab9cae0fd7d1f3a0edd09
phillip <windowsyyf@gmail.com>
2019-01-16 18:08:01 -0500

    adding files

---
+++ UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace cc150CSharp
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
