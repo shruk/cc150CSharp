e864a6f80271c129c00f36f149c0c57a63f23acc
phillip <windowsyyf@gmail.com>
2019-01-16 19:31:40 -0500

    update

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,13 +1,43 @@
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace cc150CSharp
 {
     [TestClass]
-    public class UnitTest1
+    public class InsertionSort
     {
+        public static void NormalInsertionSort(int [] arr)
+        {
+             for(int i=1 ; i<=arr.Length-1 ;++i)
+                {
+                    int val=arr[i];
+                    int j=i-1;
+
+                    while(j>=0 && arr[j]>val)
+                    {
+                        arr[j+1]=arr[j];
+                        j--;
+                    }
+                    arr[j+1]=val;
+
+                }
+
+
+        }
+
+
         [TestMethod]
         public void TestMethod1()
         {
+                int [] arr=new int[7] {7,1,2,3,4,5,6};
+                NormalInsertionSort(arr);
+
+                foreach (int i in arr)
+                {
+                    Console.Write(i);
+
+                }
+
         }
     }
 }
