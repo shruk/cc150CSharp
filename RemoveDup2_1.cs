27fc539183fbcc5526b94c8b8cb8b0537c12e431
shruk <windowsyyf@gmail.com>
2019-03-19 10:53:43 -0400

    test

--- RemoveDup2_1.cs
+++ RemoveDup2_1.cs
@@ -1,20 +1,22 @@
-using System;
-using Xunit;
-
 namespace cc150CSharp{
-    public class RotateMatrix{
-//problem:given an image by an N*N matrix,where each pixel in the image is 4 bytes. write a
-//method to rotate the image by 90 degrees. do this in place?
-//1.Listen, understand the problem, 
+    public class RemoveDup{
+//problem:remove duplicate from unsorted list
+//1.Listen, understand the problem, try not use buffer.
 //2.Example, normal small case, special case(empty string/special chars?), big enough case,
-//3.Brute force way(naive way)...gather a collection of positions and set to zero in matrix.
+//3.Brute force way(naive way)...use hashset, it is pretty simple, just iterate through and add them into HS,then reconstruct the list.
+//the author asked not to use temp buffer, meaning we need to use insertion sort(start from left)
+//compare each node to its left, put smaller one on left, then go to next
+//remove dup duing insertion.
+//note list has no indexer to access, that is a big diff from array
+//end up using 2 pointer technique, so slow runner will point to each node
+//fast runner will go through the nodes to compare the slow point.
 //4.BUD optimization. 
 //	  a: look for unused info.
 //	  b: Use a fresh example. 
 //	  c: Solve it "incorretly" will provide negative result to think it over
 //	  d: Make time vs space tradeoff. More space may save more runtime.
 //	  e: Precompute information. Sorting/compute value upfront save time in the   long run.
-//	  f: Use hash table.Yes we can load one string into hashtable, 
+//	  f: Use hash table.this solution doesn't allow temp buffer.
 //	  g: Think about the best conceivable runtime. O(M*N)
 //5.Walk Through the optimal solution found, not code yet...solidify my understanding of algorithm. Load string a into hashtable,loop through string b, get value for each position, if it is different, increase the count. if count is greater than 1, return true. if match, then remove the char from hashtable, if hashtable has only 1 item left, return true. 
 //Write code slowly and get a feel for the structure of the code. Know what the variables are and when they change.
@@ -31,47 +33,25 @@ namespace cc150CSharp{
 //	d:Small test cases.
 //	e:Special cases. Test against null or single element values, extreme cases, etc.
 //	f:When finding bugs, analyze the bug and make correction in the best place.
-   public bool IsRotation(string s1,string s2)
-   {    int i=0;
-       while(i<s1.Length)//o(n^2), not a good solution but need to test
-       {    
-           if(s2==rotate(ref s1))return true;
-           i--;
-       }
-       return false;
-
-   }
-
-   private string rotate(ref string a)
-   {
-       char [] arr=new char[a.Length];
-       arr[0]=a[a.Length-1];
-       for (int i=0;i<a.Length-1;i++)
-       {
-           arr[i+1]=a[i];
-       }
-       //trying to change string here, string internning should change the value...
-       a=new string(arr);
-       return a;
-   }
-   
+    public class Node<T>{
+        public T data;
+        public Node<T> next;
     }
+    public void RemoveDuplicate<T>(Node<T> head){
+            Node<T> slow=head;
+            Node<T> fast=head.next;
+            while(slow.next!=null)
+            {
+                //fast will go through the node for this slow
+                while (fast.next!=null)
+                {
+                    if (slow==fast){//found
+                    }
+                    fast=fast.next;
+                }
 
-    public class TestRotateMatrix{
-        private RotateMatrix _o;
-        public TestRotateMatrix(){
-            _o=new RotateMatrix();
-        }
-        [Fact]
-        public void testRotateMatrix()
-        {
-        string a="test";
-        string b="estt";
-        //Given
-        Assert.Equal(true,_o.IsRotation(a,b));
-        //When
-        
-        //Then
-        }
+                slow=slow.next;
+            }
+    }
     }
 }
