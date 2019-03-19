62767449d9ef56cf9f0b83bf8b059ed069544190
shruk <windowsyyf@gmail.com>
2019-03-18 21:31:21 -0400

    test

---
+++ ZeroMatrix1_8.cs
@@ -0,0 +1,61 @@
+using System.Collections.Generic;
+
+namespace cc150CSharp{
+    public class ZeroMatrix<Integer>{
+//0.Problem:give a M*N matrix, if one element is zero, then its column and row should be set to zero.
+//1.Listen, understand the problem, find all zero elements and zero corresponding col and row.
+//2.Example, normal small case, special case(empty string/special chars?), big enough case,
+//3.Brute force way(naive way)...gather a collection of positions and set to zero in matrix.
+//4.BUD optimization. 
+//	  a: look for unused info.
+//	  b: Use a fresh example. 
+//	  c: Solve it "incorretly" will provide negative result to think it over
+//	  d: Make time vs space tradeoff. More space may save more runtime.
+//	  e: Precompute information. Sorting/compute value upfront save time in the   long run.
+//	  f: Use hash table.Yes we can load one string into hashtable, 
+//	  g: Think about the best conceivable runtime. O(M*N)
+//5.Walk Through the optimal solution found, not code yet...solidify my understanding of algorithm. Load string a into hashtable,loop through string b, get value for each position, if it is different, increase the count. if count is greater than 1, return true. if match, then remove the char from hashtable, if hashtable has only 1 item left, return true. 
+//Write code slowly and get a feel for the structure of the code. Know what the variables are and when they change.
+//6.Implement. Write beautiful code. 
+//	 a: Modularized code. write functions pretending the function is there, fill in details later if need to.
+//   b: Error checks.
+//.  c: Use classes/structs if necessary (e.g. returning object)
+//.  d: Good varaible name. it is OK to use abbreviation.
+//.  e: Just write beautiful code (need to practice)
+//7.Test. 
+//	a: Conceptual test. reading and analyzing each line, does the code do what you think it does?
+//	b:Weird looking code. Double check the line of code that is weird.
+//	c:Hot spots. know wht things are likely to cause problem. recursive code. Integer division, Null nodes in binary trees. The start and end of iteration throught liked list.
+//	d:Small test cases.
+//	e:Special cases. Test against null or single element values, extreme cases, etc.
+//	f:When finding bugs, analyze the bug and make correction in the best place.       
+    public int[][] setZero<T>(int [][] matrix,int m, int n){
+        //2D array was set to have m rows and n cols
+        HashSet<int> xs=new HashSet<int>();
+        HashSet<int> ys=new HashSet<int>();
+        for(int i=0;i<m;i++){//O(m*n)
+            for(int j=0;j<n;j++){
+                if (matrix[i][j]==0)
+                {
+                    xs.Add(i);ys.Add(j);
+                }
+            }
+        }
+        foreach( int i in xs)
+        {
+            for(int j=0;j<n;j++)
+            {
+                matrix[i][j]=0;
+            }
+        }
+        foreach( int j in ys)
+        {
+            for(int i=0;i<m;i++)
+            {
+                matrix[i][j]=0;
+            }
+        }
+        return matrix;
+    }
+    }
+}
